dcb36c920d7a5dd47d09fc42b7a4489d4ba25ac6
Andres <andresmholguin@gmail.com>
2025-03-27 17:00:31 -0500

    Schema mongoose

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
+import colors from "colors";
 
 export const connectDB = async () => {
   try {
-    const url =
-      "mongodb+srv://root:sReKaXrcO7FLNOik@cluster0.wqfbfii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-    const conection = await mongoose.connect(url);
+    const conection = await mongoose.connect(process.env.MONGO_URI);
 
-    const url2 = `${conection.connection.host}:${conection.connection.port}`;
-    console.log(url2);
-    console.log("MongoDB connected");
+    const url = `${conection.connection.host}:${conection.connection.port}`;
+    console.log(url);
+    console.log(colors.bgCyan.green.bold("MongoDB connected"));
   } catch (error) {
-    console.log(error.message);
+    console.log(colors.bgRed.white.bold(error.message));
     process.exit(1);
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import colors from "colors";
 import server from "./server";
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log("Server init in port: " + port);
+  console.log(colors.bgGreen.gray("Server init in port: ") + port);
 });
